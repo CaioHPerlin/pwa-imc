7ec6496ae35e15ded2745a2c971a7447558b7536
Caio H Perlin <caiohperlin@gmail.com>
2023-08-21 00:49:05 -0400

    fix: modify imc calculator

--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
 const form = document.getElementById('calc')
-
-const inputName = document.getElementById('in-name')
-const inputAge = document.getElementById('in-age')
-const inputSexM = document.getElementById('masc')
-const inputSexF = document.getElementById('fem')
-const inputHeight = document.getElementById('in-height')
-const inputWeight = document.getElementById('in-weight')
-
-const outName = document.getElementById('out-name')
-const outResult = document.getElementById('out-result')
-const outImc = document.getElementById('out-imc')
+const output = document.getElementsByClassName('output')[0]
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    const imc = inputWeight.value / ((inputHeight.value/100)**2)
-    console.log(imc)
-    outImc.innerHTML = imc
-    outName.innerHTML = inputName.value
+
+    const imc = (form.weight.value / ((form.height.value/100)**2)).toFixed(2)
+
+    document.getElementById('out-imc').innerHTML = imc;
+    document.getElementById('out-name').innerHTML = form.name.value;
+
     let result = ''
     if(imc < 18.5) result = 'Abaixo do peso normal'; else
     if(imc < 24.9) result = 'Peso normal'; else
     if(imc < 29.9) result = 'Excesso de peso'; else
     if(imc < 34.9) result = 'Obesidade classe I'; else
     if(imc < 39.9) result = 'Obesidade classe II'; else result = 'Obesidade classe III';
-    outResult.innerHTML = result;
+    document.getElementById('out-result').innerHTML = result;
+
+    output.style.display = "block"
 });
